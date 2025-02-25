fe8c11e05879a32cda79549a2665744de5a47976
Mikhail Guskov <ringill@mail.ru>
2025-02-26 00:13:25 +0300

    вернул исходное состояние тестового файла

--- CalculatorTests.cs
+++ CalculatorTests.cs
@@ -1,21 +1,14 @@
 using NUnit.Framework;
-/// <summary>
-/// Класс `CalculatorTests` содержит тесты для проверки функциональности калькулятора.
-/// </summary>
+
 public class CalculatorTests
 {
-/// <summary>
-/// Метод `Add_TwoNumbers_ReturnsSum` принимает на вход два числа и возвращает их сумму.
-/// </summary
-[Test]
+  [Test]
   public void Add_TwoNumbers_ReturnsSum()
   {
     // Test logic
   }
-/// <summary>
-/// Метод `Add_TwoNumbers_ReturnsSum` принимает два целых числа `a` и `b` и возвращает их сумму как результат вычисления.
-/// </summary
-[Test]
+
+  [Test]
   [Category("Math")]
   [Description("Проверка сложения чисел")]
   public void Add_TwoNumbers_ReturnsSum(int a, int b)
@@ -23,15 +16,11 @@ public class CalculatorTests
     // ...
   }
 }
-/// <summary>
-/// Класс `CalculatorTests1` предназначен для тестирования функциональности калькулятора.
-/// </summary>
+
 public class CalculatorTests1
 {
-/// <summary>
-/// Метод `Add_TwoNumbers_ReturnsSum1` предназначен для сложения двух чисел и возвращает их сумму.
-/// </summary
-[Test]
+
+  [Test]
   public void Add_TwoNumbers_ReturnsSum1()
   {
     // Test logic
